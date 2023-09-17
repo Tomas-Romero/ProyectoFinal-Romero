1d1d87c9a43dccc0cd5457e8d57deb6368c013f8
Tomas Romero <Tomasromero200310@gmail.com>
2023-09-17 13:07:17 -0300

    Home page funcionando correctamente

--- javascript/main.js
+++ javascript/main.js
@@ -808,7 +808,6 @@ class ShoppingCart {
     addToCart(elementID){
         const productToAdd = products[elementID];
         if (productToAdd) {
-            
             this.productsCart.push(productToAdd);
         } else {
             console.log(`El producto con el ID ${elementId} no existe.`); //Is to check the error, later I will add an sweetalert
@@ -838,7 +837,7 @@ class ShoppingCart {
 }
 //Create the shopping cart
 let shoppingCart = new ShoppingCart()
-
+let counterCart = document.getElementById("counter-cart")
 //Code to the function of the buttons "buy" for the products
 function createEventBuyButtons(){
     const botonesCompra = document.querySelectorAll(".button_buy");
@@ -846,10 +845,12 @@ function createEventBuyButtons(){
         boton.addEventListener("click", () => {
             const productId = boton.parentElement.getAttribute("data-product-id");
             shoppingCart.addToCart(productId);
+            counterCart.textContent = shoppingCart.productsCart.length.toString()
+            actualizarMenuDesplegable();
+            guardarCarritoEnSessionStorage(shoppingCart);
         });
     });
 }
-
 //Code for the filters and products section
 let filtersContainer = document.getElementById("home_section_filter")
 filtersContainer.innerHTML += ""
@@ -890,3 +891,28 @@ checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', mostrarProductosFiltrados);
 });
 mostrarProductosFiltrados();
+//Code to add a menu when mouse is on shopping cart icon
+
+
+function actualizarMenuDesplegable() {
+    let dropdownCartMenu = document.getElementById("dropdown-menu-cart")
+    let textoagregado = ""
+    shoppingCart.productsCart.forEach(element =>{
+        textoagregado += `<div class="white_text d-flex flex-row justify-content-between lg_bottom_bordered mb-3">
+        <span class="dropdown-item-text white_text w-45">${element.productName}</span>
+        <span class="dropdown-item-text white_text w-45">USD$${element.price}</span></div>`
+    } )
+    dropdownCartMenu.innerHTML = textoagregado + `<div class="d-flex flex-row justify-content-between p-4">
+    <h4 class="white_text">Total: </h4><span class="white_text">USD$${shoppingCart.calculateTotal()}</span></div>`
+}
+
+function guardarCarritoEnSessionStorage(carrito) {
+    sessionStorage.setItem('carrito', JSON.stringify(carrito));
+  }
+  // Obtener el carrito desde sessionStorage
+function obtenerCarritoDesdeSessionStorage() {
+    const carrito = sessionStorage.getItem('carrito');
+    return carrito ? JSON.parse(carrito) : [];
+}
+const carrito = obtenerCarritoDesdeSessionStorage();
+// Ejemplo de uso:
